bade025053908e8c06cdee4824f634dd33772883
swind <swind@uni-muenster.de>
2025-07-28 17:11:27 +0200

    add variables

--- R/globals.R
+++ R/globals.R
@@ -1,11 +1,13 @@
 utils::globalVariables(c(
-    ".", "Amplicon", "Chromosome", "Exon", "Gene", "Genotype", 
-    "Genotype Quality", "Het", "Hom", "ID", "Missing", 
-    "Normalized_mean_read_counts_per_amplicon", "Protein", "Total", 
-    "Transcript.ID", "VAF", "WT", "all_of", "any_of", "barcode", 
-    "box", "cluster", "data", "download.file", "exonsBy", "hcl", 
-    "head", "id", "ideoCyto", "kmeans", "median", "mtext", 
-    "na.omit", "read.delim", "reorder", "rowname", "seqlen", 
-    "setNames", "starts_with", "tooltip", "transcript", "value", 
-    "variable", "x", "y"
+    ".", "Amplicon", "Chromosome", "Exon", "Gene", "Genotype",
+    "Genotype Quality", "Het", "Hom", "ID", "Missing",
+    "Normalized_mean_read_counts_per_amplicon", "Protein", "Total",
+    "Transcript.ID", "VAF", "WT", "all_of", "any_of", "barcode",
+    "box", "cluster", "data", "download.file", "exonsBy", "hcl",
+    "head", "id", "ideoCyto", "kmeans", "median", "mtext",
+    "na.omit", "read.delim", "reorder", "rowname", "seqlen",
+    "setNames", "starts_with", "tooltip", "transcript", "value",
+    "variable", "x", "y", "n_bases_r1", "n_bases_r2", "n_bases_r1_q30",
+    "n_bases_r2_q30", "n_bases_r1_q30", "group", "n_cell_barcode_bases",
+    "n_cell_barcode_bases_q30", "prcomp", "everything", "Type"
 ))
